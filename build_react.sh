2fe93006f541e6a4a2ccb8ae10a9f73eab3a6df9
abhishek095859 <abhishek.narmula@minfytech.com>
2025-09-15 09:46:28 +0530

    Update build_react.sh

--- build_react.sh
+++ build_react.sh
@@ -3,7 +3,5 @@ set -euo pipefail
 
 # # The build is produced in CodeBuild and copied by CodeDeploy per appspec.yml.
 # # Here we just ensure Nginx is serving the latest files.
-# systemctl restart nginx
-# echo "Nginx restarted. Deployment completed."
-npm install
-npm run build
+systemctl restart nginx
+echo "Nginx restarted. Deployment completed."
